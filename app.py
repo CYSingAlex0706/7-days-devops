7197143b9d7b094902d982d411c36cb4f35ede8b
CYSingAlex0706 <97864075+CYSingAlex0706@users.noreply.github.com>
2025-04-02 00:33:28 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,15 +1,3 @@
-# from flask import Flask
-
-# app = Flask(__name__)
-
-# @app.route('/')
-# def home():
-#     return "Hello, World!"
-
-# if __name__ == '__main__':
-#     app.run(debug=True)
-
-
 from flask import Flask, render_template
 
 app = Flask(__name__)
@@ -19,4 +7,4 @@ def home():
     return render_template('index.html')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000)
